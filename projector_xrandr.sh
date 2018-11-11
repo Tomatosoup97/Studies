6dc78f545ad6e0f535d7545199e5e1f2283c4456
Mateusz Urbańczyk <mu@qed.ai>
2018-11-11 11:35:38 +0100

    General scripts for projector config and presentation tty

--- projector_xrandr.sh
+++ projector_xrandr.sh
@@ -1,3 +1,3 @@
-# xrandr --output eDP1 --auto --output HDMI1 --same-as eDP1
+xrandr --output eDP1 --mode 1024x768 --output HDMI1 --auto
 xrandr --output eDP1 --mode 1024x768 --output HDMI1 --same-as eDP1
 
