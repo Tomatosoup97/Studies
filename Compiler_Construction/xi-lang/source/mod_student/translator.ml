88e087c82ea487d23724049dc7b89459d743c2a3
Mateusz Urbańczyk <mu@qed.ai>
2018-11-27 11:24:41 +0100

    [CC] First rules for generating CFG

--- Compiler_Construction/xi-lang/source/mod_student/translator.ml
+++ Compiler_Construction/xi-lang/source/mod_student/translator.ml
@@ -117,12 +117,11 @@ module Make() = struct
     let i32_1 = Int32.of_int 1
 
     let binop_instr res_reg lhs rhs = function
-      | Ast.BINOP_Add -> I_Add (res_reg, lhs, rhs)
       | Ast.BINOP_Sub -> I_Sub (res_reg, lhs, rhs)
       | Ast.BINOP_Mult -> I_Mul (res_reg, lhs, rhs)
       | Ast.BINOP_Div -> I_Div (res_reg, lhs, rhs)
       | Ast.BINOP_Rem -> I_Rem (res_reg, lhs, rhs)
-      | _ -> failwith "not yet implemented"
+      | _ -> failwith "Unknown BINOP operator"
 
     let unop_instr res_reg sub = function
       | Ast.UNOP_Not -> I_Not (res_reg, sub)
@@ -133,15 +132,36 @@ module Make() = struct
       | Ast.EXPR_Char {value; _} ->
           current_bb, E_Int (Int32.of_int @@ Char.code value)
 
-      | Ast.EXPR_Bool {b; _} ->
-          current_bb, E_Int (if b then 1 else 0)
+      | Ast.EXPR_Bool {value; _} ->
+          current_bb, E_Int (Int32.of_int (if value then 1 else 0))
+
+      | Ast.EXPR_String {value; _} ->
+          let str_reg = allocate_register () in
+          let str_len = Int32.of_int (String.length value) in
+          append_instruction current_bb @@ I_NewArray (str_reg, E_Int str_len);
+          (* TODO: do I need to insert one char by one with I_StoreArray? *)
+          current_bb, E_Reg str_reg
 
       | Ast.EXPR_Id {id; _} ->
           current_bb, E_Reg (Environment.lookup_var id env)
 
       | Ast.EXPR_Binop {lhs; rhs; op=Ast.BINOP_Or; _}
       | Ast.EXPR_Binop {lhs; rhs; op=Ast.BINOP_And; _} ->
+          (* translate_condition? *)
           failwith "not yet implemented"
+
+      | Ast.EXPR_Binop {lhs; rhs; op=Ast.BINOP_Add; tag; _} ->
+          let res_reg = allocate_register () in
+          let current_bb, lhs_res = translate_expression env current_bb lhs in
+          let current_bb, rhs_res = translate_expression env current_bb rhs in
+          (match Hashtbl.find node2type tag with
+            | TP_Int -> append_instruction current_bb @@
+                        I_Add (res_reg, lhs_res, rhs_res)
+            | TP_Array t -> append_instruction current_bb @@
+                            I_Concat (res_reg, lhs_res, rhs_res)
+            | _ -> failwith "Cannot add given type, typechecker should have failed!"
+          );
+          current_bb, E_Reg res_reg
 
       | Ast.EXPR_Binop {lhs; rhs; op; _} ->
           let res_reg = allocate_register () in
@@ -157,7 +177,25 @@ module Make() = struct
           current_bb, E_Reg res_reg
 
       | Ast.EXPR_Index {expr; index; _} ->
+          let res_reg = allocate_register () in
+          let current_bb, xs = translate_expression env current_bb expr in
+          let current_bb, index = translate_expression env current_bb index in
+          append_instruction current_bb @@ I_LoadArray (res_reg, xs, index);
+          current_bb, E_Reg res_reg
+
+      | Ast.EXPR_Struct {elements; _} ->
+          (* TODO
+           * for each element translate_expression and append result to xs
+           * allocate len(elements) array
+           * for each x in xs StoreArray?
+           *)
           failwith "not yet implemented"
+
+      | Ast.EXPR_Length {arg; _} ->
+          let res_reg = allocate_register () in
+          let current_bb, arg_res = translate_expression env current_bb arg in
+          append_instruction current_bb @@ I_Length (res_reg, arg_res);
+          current_bb, E_Reg res_reg
 
       | _ ->
           failwith "not yet implemented"
@@ -183,14 +221,71 @@ module Make() = struct
     (* --------------------------------------------------- *)
 
     let rec translate_statement env current_bb = function
+      | Ast.STMT_Assign {lhs; rhs; _} ->
+          (match lhs with
+            | Ast.LVALUE_Id {id; _} ->
+                (* TODO: recheck *)
+                let var = Environment.lookup_var id env in
+                let current_bb, rhs_res = translate_expression env current_bb rhs in
+                append_instruction current_bb @@ I_Move (var, rhs_res)
+
+            | Ast.LVALUE_Index {sub; index; _} ->
+                let current_bb, xs = translate_expression env current_bb sub in
+                let current_bb, index = translate_expression env current_bb index in
+                let current_bb, rhs_res = translate_expression env current_bb rhs in
+                append_instruction current_bb @@ I_StoreArray (xs, index, rhs_res)
+          );
+          current_bb
+
+      | Ast.STMT_VarDecl {var; init; _} ->
+          (* TODO case when var is array *)
+          (* TODO: what to do with ext_env? nothing? *)
+          (*
+          (match Hashtbl.find node2type tag with
+            | TP_Int -> ();
+            | TP_Array -> append_instruction current_bb @@ I_Move (var_reg, init_res)
+          )
+          *)
+          (match init with
+            | Some init ->
+                let current_bb, init_res = translate_expression env current_bb init in
+                let ext_env, var_reg = bind_var_declaration env var in
+                append_instruction current_bb @@ I_Move (var_reg, init_res);
+                current_bb
+            | None ->
+                (* TODO: We do nothing here? *)
+                let ext_env, var_reg = bind_var_declaration env var in
+                current_bb
+          )
+
+      | Ast.STMT_If { cond; then_branch; else_branch; _} ->
+          let bb_else = allocate_block () in
+          let bb_then = translate_condition env current_bb bb_else cond in
+          let bb_then' = translate_statement env bb_then then_branch in
+          let bb_merge = allocate_block() in
+          (match else_branch with
+            | Some else_branch ->
+                let bb_else' = translate_statement env bb_else else_branch in
+                set_jump bb_else' bb_merge
+            | None ->
+                set_jump bb_else bb_merge
+          );
+          set_jump bb_then' bb_merge;
+          bb_merge
+
+      | Ast.STMT_Block body ->
+          let _, current_bb = translate_block env current_bb body in
+          current_bb
+
       | _ ->
         failwith "not yet implemented"
-
 
     and translate_block env current_bb (Ast.STMTBlock {body; _}) =
       failwith "not yet implemented"
 
-    let bind_var_declaration env vardecl =
+    (* TODO: it was let, not and, does it mean that we shouldn't use it in
+    * translate statement? *)
+    and bind_var_declaration env vardecl =
       let r = allocate_register () in
       let env = Environment.add_var (Ast.identifier_of_var_declaration vardecl) r env in
       env, r
