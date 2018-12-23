b76a10eda95ac1ff185c4e63d6f263918f5a5647
Mateusz Urbańczyk <mu@qed.ai>
2018-12-23 15:21:27 +0100

    [CC] Simplify and select functions

--- Compiler_Construction/xi-lang/source/mod_student/regalloc.ml
+++ Compiler_Construction/xi-lang/source/mod_student/regalloc.ml
@@ -64,6 +64,11 @@ module Make(Toolbox:Iface.COMPILER_TOOLBOX) = struct
      *  Pomocnicze funkcje
      *)
 
+    let log_str_list xs =
+      Logger.extra_debug begin fun () ->
+        logf "%s" (String.concat " " xs)
+      end
+
     let loop name f =
       let rec iter i =
         logf "Starting iteration %s %u" name i;
@@ -105,7 +110,19 @@ module Make(Toolbox:Iface.COMPILER_TOOLBOX) = struct
      * Simplify phase
      *)
 
+    let vertex_neighbours infg v =
+      let add_nb w1 w2 acc =
+        if      w1 == v then w1 :: acc
+        else if w2 == v then w2 :: acc
+        else            acc
+      in
+      RegGraph.fold_edges add_nb infg []
+
     let graph_vertexes_list g = RegGraph.fold_vertex List.cons g []
+
+    let reg_deg infg = function
+        | (REG_Tmp _) as v -> RegGraph.out_degree infg v
+        | _ -> max_int
 
     let min_in_graph infg compare =
         match graph_vertexes_list infg with
@@ -131,14 +148,15 @@ module Make(Toolbox:Iface.COMPILER_TOOLBOX) = struct
           | None -> failwith "No available candidates for spill!"
 
     let simplify scosts infg =
+        logf "Run simplify";
         let nodes = Stack.create () in
-        let compare_degrees infg v1 v2 =
-          RegGraph.out_degree infg v1 < RegGraph.out_degree infg v2
-        in
+        let compare_degrees infg v1 v2 = reg_deg infg v1 < reg_deg infg v2 in
         let rec simplify_loop = fun () ->
           match graph_vertexes_list infg, min_in_graph infg compare_degrees with
-            | _ :: _, Some (REG_Tmp _ as min_v)  ->
-              if (RegGraph.out_degree infg min_v) < number_of_available_registers
+            | v :: vs, Some (REG_Tmp _ as min_v)  ->
+              let min_v_deg = RegGraph.out_degree infg min_v in
+              log_str_list ["Stacking register"; (Ir_utils.string_of_reg min_v); "of degree"; (string_of_int min_v_deg)];
+              if min_v_deg < number_of_available_registers
               then (
                 Stack.push min_v nodes;
                 RegGraph.remove_vertex infg min_v;
@@ -150,14 +168,52 @@ module Make(Toolbox:Iface.COMPILER_TOOLBOX) = struct
                 RegGraph.remove_vertex infg min_v;
                 simplify_loop ()
               )
-            | _, _ -> nodes
+            | _, _ ->
+                logf "No more temporary registers in interference-graph";
+                nodes
         in simplify_loop ()
 
     (* ------------------------------------------------------------------------
      *  Select phase
      *)
 
-    let select = failwith "select not yet implemented"
+    let fst_seq_num = function
+      (* Given an sorted array return first sequential element that is not
+       * in the list. Example: [0, 1, 2, 4] -> 3 *)
+      | [] -> 0
+      | x :: xs ->
+        let rec aux prev = (function
+          | [] -> prev + 1
+          | x :: xs -> if x == (prev + 1) then aux x xs else prev + 1)
+        in aux x xs
+
+    let select_color infg v =
+      let nbs = vertex_neighbours infg v in
+      log_str_list ["Picking color for"; Ir_utils.string_of_reg v];
+      let get_color v = Hashtbl.find register2color_assignment v in
+      let ncolors = List.sort compare (List.map get_color nbs) in
+      log_str_list (["Neighbours have colors:"] @ List.map string_of_int ncolors);
+      let color = fst_seq_num ncolors in
+      log_str_list ["Picking color:"; string_of_int color];
+      Hashtbl.add register2color_assignment v color;
+      color
+
+    let select potential_spills infg =
+      logf "Run select";
+      let rec aux actual_spills =
+        if Stack.is_empty potential_spills
+        then (
+          logf "No more potential spills";
+          actual_spills
+        ) else
+          let v = Stack.pop potential_spills in
+          RegGraph.add_vertex infg v;  (* restore vertex *)
+          if select_color infg v >= number_of_available_registers (* TODO: GT or GTE? *)
+          then (
+            log_str_list ["Actual spill of register"; Ir_utils.string_of_reg v];
+            aux (actual_spills @ [v])
+          ) else aux actual_spills
+      in aux []
 
     (* ------------------------------------------------------------------------
      *  build-coalesce loop
@@ -176,6 +232,7 @@ module Make(Toolbox:Iface.COMPILER_TOOLBOX) = struct
      *)
 
     let single_pass () =
+      logf "Run single_pass";
       let init () = begin
           (* resetujemy robocze tablice *)
           Hashtbl.reset register2color_assignment;
@@ -189,19 +246,24 @@ module Make(Toolbox:Iface.COMPILER_TOOLBOX) = struct
       let spill_costs = measure "spillcosts" (fun () -> compute_spill_costs infg) in
       (* uruchom fazę simplify/select/spill *)
       let simplify_stack = simplify spill_costs infg in
-
-      (* unit na potrzeby interfejsu pomocniczej funkcji loop *)
-      (), true
+      match select simplify_stack infg with
+        | [] -> (), false
+        | xs -> (), spill xs
 
     (* ------------------------------------------------------------------------
      *  Budowanie mapowania rejestrów
      *)
 
     let build_register_assignment () =
+      logf "Run build_register_assignment";
       let register_assignment : (reg, reg) Hashtbl.t = Hashtbl.create 513 in
-      failwith "build_register_assignment not yet implemented";
       (* Przejdz tablice register2color_assignment i uzupełnij prawidłowo
        * tablicę register_assignment *)
+      let f reg c =
+        let hard_reg = Hashtbl.find color2register_assignment c in
+        Hashtbl.add register_assignment reg hard_reg;
+      in
+      Hashtbl.iter f register2color_assignment;
       register_assignment
 
     (* ------------------------------------------------------------------------
